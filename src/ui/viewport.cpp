2c576f8b5620b7168ba3070000cbf8d675c09953
JoeyChen <joeyskeys>
2018-07-29 21:06:23 +0800

    fix linux build problems, add dynamicmesh collapse

--- src/ui/viewport.cpp
+++ src/ui/viewport.cpp
@@ -51,7 +51,8 @@ void QtViewport::initializeGL()
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	
 	//m_program->load("E:/work/repos/vp/src/shaders/", "basic");
-	m_program->load("G:/WorkSpace/repos/vp/src/shaders/", "basic");
+	//m_program->load("G:/WorkSpace/repos/vp/src/shaders/", "basic");
+    m_program->load("/home/joey/Desktop/workspace/repos/self/vp/src/shaders/", "basic");
 	m_prog = m_program->getProgram();
 
 	m_proj_loc = glGetUniformLocation(m_prog, "proj");
